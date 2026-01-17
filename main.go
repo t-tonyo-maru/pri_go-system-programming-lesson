5da97657ad7e54473fef0147b05a7dd53ff2c2ef
t-tonyo-maru <tonyomaru108@gmail.com>
2026-01-17 16:43:36 +0900

    update

--- main.go
+++ main.go
@@ -1,6 +1,89 @@
 package main
 
 // =======================================================
+// ## 3.5.4 PNG 画像に秘密のテキストを入れてみる
+// import (
+// 	"bytes"
+// 	"encoding/binary"
+// 	"fmt"
+// 	"hash/crc32"
+// 	"io"
+// 	"os"
+// )
+
+// func dumpChunk(chunk io.Reader) {
+// 	var length int32
+// 	binary.Read(chunk, binary.BigEndian, &length)
+// 	buffer := make([]byte, 4)
+// 	chunk.Read(buffer)
+// 	fmt.Printf("chunk '%v' (%d bytes)\n", string(buffer), length)
+// }
+
+// func readChunks(file *os.File) []io.Reader {
+// 	// チャンクを格納する配列
+// 	var chunks []io.Reader
+
+// 	// 最初の 8 バイトを飛ばす
+// 	file.Seek(8, 0)
+// 	var offset int64 = 8
+
+// 	for {
+// 		var length int32
+// 		err := binary.Read(file, binary.BigEndian, &length)
+// 		if err == io.EOF {
+// 			break
+// 		}
+// 		chunks = append(chunks,
+// 			io.NewSectionReader(file, offset, int64(length)+12))
+// 		// 次のチャンクの先頭に移動
+// 		// 現在位置は長さを読み終わった箇所なので
+// 		// チャンク名（4バイト）+データ長+CRC(4バイト)先に移動
+// 		offset, _ = file.Seek(int64(length+8), 1)
+// 	}
+// 	return chunks
+// }
+
+// func textChunk(text string) io.Reader {
+// 	byteText := []byte(text)
+// 	crc := crc32.NewIEEE()
+// 	var buffer bytes.Buffer
+// 	binary.Write(&buffer, binary.BigEndian, int32(len(byteText)))
+
+// 	// CRC 計算とバッファへの書き込みを同時に行う MultiWriter
+// 	writer := io.MultiWriter(&buffer, crc)
+// 	io.WriteString(writer, "teXt") // 2バイト目の5ビット目を立てる
+// 	writer.Write(byteText)
+// 	binary.Write(&buffer, binary.BigEndian, crc.Sum32())
+// 	return &buffer
+// }
+
+// func main() {
+// 	file, err := os.Open("PNG_transparency_demonstration_1.png")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer file.Close()
+
+// 	newFile, err := os.Create("PNG_transparency_demonstration_secret.png")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	defer newFile.Close()
+// 	chunks := readChunks(file)
+// 	// シグニチャ書き込み
+// 	io.WriteString(newFile, "\x89PNG\r\n\x1a\n")
+// 	// 先頭に必要な IHDR チャンクを書き込み
+// 	io.Copy(newFile, chunks[0])
+// 	// テキストチャンクを追加
+// 	io.Copy(newFile, textChunk("Lamda Note++"))
+// 	// 残りのチャンクを追加
+// 	for _, chunk := range chunks[1:] {
+// 		io.Copy(newFile, chunk)
+// 	}
+// }
+
+// =======================================================
 // ## 3.5.2 PNG ファイルを分析してみる
 // import (
 // 	"encoding/binary"
