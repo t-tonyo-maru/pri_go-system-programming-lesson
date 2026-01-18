27f5e1a5d57db1f96ba991e32a016bf7896e5fd5
t-tonyo-maru <tonyomaru108@gmail.com>
2026-01-18 23:11:59 +0900

    update

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("start sub()")
+	// 終了を受け取るためのチャネル
+	done := make(chan bool)
+	go func() {
+		fmt.Println("sub() is finished")
+		// 終了を通知
+		done <- true
+	}()
+	// 終了を待つ
+	<-done
+	fmt.Println("all tasks are finidhed.")
 }
+
+// =======================================================
+// ## 4.2.1 チャネルの使用方法
 
 // =======================================================
 // ## 4.2 チャネル
