33c626d877f8b456d45c99c47dfaa9c7d9c5e8ad
t-tonyo-maru <tonyomaru108@gmail.com>
2026-01-18 22:55:41 +0900

    updat

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+// =======================================================
+// ## 4.1 goroutine
+// go というキーワードをつけて実行すれば goroutine が作られて並列実行される
+// import (
+// 	"fmt"
+// 	"time"
+// )
 
-}
+// func sub() {
+// 	fmt.Println("sub() is running")
+// 	time.Sleep(time.Second)
+// 	fmt.Println("sub() is finished")
+// }
+
+// func main() {
+// 	fmt.Println("start sub()")
+// 	go sub()
+// 	time.Sleep(5 * time.Second)
+// 	fmt.Println("end sub()")
+// }
