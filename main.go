22b811d71ffb04e516e47528ef606e4772706212
t-tonyo-maru <tonyomaru108@gmail.com>
2026-01-21 23:43:16 +0900

    update

--- main.go
+++ main.go
@@ -1,75 +1,77 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"io"
-	"net"
-	"net/http"
-	"net/http/httputil"
-	"strings"
-	"time"
-)
-
-func main() {
-	listener, err := net.Listen("tcp", "localhost:8888")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Server is running at localhost:8888")
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			panic(err)
-		}
-		go func() {
-			defer conn.Close()
-			fmt.Printf("Accept %v\n", conn.RemoteAddr())
-			// Accept 後のソケットで何度も応答を返すためループ
-			for {
-				// タイムアウトを設定
-				conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-				// リクエストを読み込む
-				request, err := http.ReadRequest(bufio.NewReader(conn))
-				if err != nil {
-					// タイムアウトもしくはソケットクローズ時は終了
-					// それ以外はエラーにする
-					neterr, ok := err.(net.Error) // ダウンキャスト
-					if ok && neterr.Timeout() {
-						fmt.Println("Timeout")
-						break
-					} else if err == io.EOF {
-						break
-					}
-					panic(err)
-				}
-
-				// リクエストを表示
-				dump, err := httputil.DumpRequest(request, true)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println(string(dump))
-				content := "Hello World\n"
-
-				// レスポンスを書き込む
-				// HTTP/1.1 かつ、ContentLength の設定が必要
-				response := http.Response{
-					StatusCode:    200,
-					ProtoMajor:    1,
-					ProtoMinor:    1,
-					ContentLength: int64(len(content)),
-					Body:          io.NopCloser(strings.NewReader(content)),
-				}
-				response.Write(conn)
-			}
-		}()
-	}
-}
+// =======================================================
+// ## 6.6.2 Keep-Alive 対応の HTTP クライアント
 
 // =======================================================
 // ## 6.6.1 Keep-Alive 対応の HTTP サーバー
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"io"
+// 	"net"
+// 	"net/http"
+// 	"net/http/httputil"
+// 	"strings"
+// 	"time"
+// )
+
+// func main() {
+// 	listener, err := net.Listen("tcp", "localhost:8888")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println("Server is running at localhost:8888")
+
+// 	for {
+// 		conn, err := listener.Accept()
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		go func() {
+// 			defer conn.Close()
+// 			fmt.Printf("Accept %v\n", conn.RemoteAddr())
+// 			// Accept 後のソケットで何度も応答を返すためループ
+// 			for {
+// 				// タイムアウトを設定
+// 				conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+// 				// リクエストを読み込む
+// 				request, err := http.ReadRequest(bufio.NewReader(conn))
+// 				if err != nil {
+// 					// タイムアウトもしくはソケットクローズ時は終了
+// 					// それ以外はエラーにする
+// 					neterr, ok := err.(net.Error) // ダウンキャスト
+// 					if ok && neterr.Timeout() {
+// 						fmt.Println("Timeout")
+// 						break
+// 					} else if err == io.EOF {
+// 						break
+// 					}
+// 					panic(err)
+// 				}
+
+// 				// リクエストを表示
+// 				dump, err := httputil.DumpRequest(request, true)
+// 				if err != nil {
+// 					panic(err)
+// 				}
+// 				fmt.Println(string(dump))
+// 				content := "Hello World\n"
+
+// 				// レスポンスを書き込む
+// 				// HTTP/1.1 かつ、ContentLength の設定が必要
+// 				response := http.Response{
+// 					StatusCode:    200,
+// 					ProtoMajor:    1,
+// 					ProtoMinor:    1,
+// 					ContentLength: int64(len(content)),
+// 					Body:          io.NopCloser(strings.NewReader(content)),
+// 				}
+// 				response.Write(conn)
+// 			}
+// 		}()
+// 	}
+// }
 
 // =======================================================
 // ## 6.5.2 TCP ソケットを使った HTTP クライアント
