b29d664964eae5c4bf836e126c1ad79d15dd2690
t-tonyo-maru <tonyomaru108@gmail.com>
2026-01-21 23:26:05 +0900

    update

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8888")
+	if err != nil {
+		panic(err)
+	}
+
+	request, err := http.NewRequest("GET", "http://localhost:8888", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	request.Write(conn)
+	response, err := http.ReadResponse(bufio.NewReader(conn), request)
+	if err != nil {
+		panic(err)
+	}
+
+	dump, err := httputil.DumpResponse(response, true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(dump))
+}
+
 // =======================================================
 // ## 6.5.2 TCP ソケットを使った HTTP クライアント
 
