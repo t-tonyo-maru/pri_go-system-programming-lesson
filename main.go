53f1e663a7141b1904d0224d1b7a0ea9d3d09183
t-tonyo-maru <tonyomaru108@gmail.com>
2026-01-21 23:19:07 +0900

    update

--- main.go
+++ main.go
@@ -1,49 +1,51 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"io"
-	"net"
-	"net/http"
-	"net/http/httputil"
-	"strings"
-)
-
-func main() {
-	listener, err := net.Listen("tcp", "localhost:8888")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Server is running at localhost:8888")
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			panic(err)
-		}
-		go func() {
-			fmt.Printf("Accept %v\n", conn.RemoteAddr())
-			// リクエストを読み込む
-			request, err := http.ReadRequest(bufio.NewReader(conn))
-			if err != nil {
-				panic(err)
-			}
-			dump, err := httputil.DumpRequest(request, true)
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println(string(dump))
-			response := http.Response{
-				StatusCode: 200,
-				ProtoMajor: 1,
-				Body:       io.NopCloser(strings.NewReader("Hello World\n")),
-			}
-			response.Write(conn)
-			conn.Close()
-		}()
-	}
-}
+// =======================================================
+// ## 6.5.2 TCP ソケットを使った HTTP クライアント
 
 // =======================================================
 // ## 6.5.1 TCP ソケットを使った HTTP サーバー
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"io"
+// 	"net"
+// 	"net/http"
+// 	"net/http/httputil"
+// 	"strings"
+// )
+
+// func main() {
+// 	listener, err := net.Listen("tcp", "localhost:8888")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println("Server is running at localhost:8888")
+
+// 	for {
+// 		conn, err := listener.Accept()
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		go func() {
+// 			fmt.Printf("Accept %v\n", conn.RemoteAddr())
+// 			// リクエストを読み込む
+// 			request, err := http.ReadRequest(bufio.NewReader(conn))
+// 			if err != nil {
+// 				panic(err)
+// 			}
+// 			dump, err := httputil.DumpRequest(request, true)
+// 			if err != nil {
+// 				panic(err)
+// 			}
+// 			fmt.Println(string(dump))
+// 			response := http.Response{
+// 				StatusCode: 200,
+// 				ProtoMajor: 1,
+// 				Body:       io.NopCloser(strings.NewReader("Hello World\n")),
+// 			}
+// 			response.Write(conn)
+// 			conn.Close()
+// 		}()
+// 	}
+// }
