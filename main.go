631062f339ae95ae72f34e5c91ff2e9533d26978
t-tonyo-maru <tonyomaru108@gmail.com>
2026-01-24 14:22:21 +0900

    update

--- main.go
+++ main.go
@@ -1,5 +1,90 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+	"time"
+)
+
+// クライアントは gzip を受け入れ可能化？
+func isGZipAcceptable(request *http.Request) bool {
+	return strings.Index(
+		strings.Join(request.Header["Accept-Encoding"], ","),
+		"gzip",
+	) != -1
+}
+
+// 1 セッションの処理をする
+func processSession(conn net.Conn) {
+	fmt.Printf("Accpet %v\n", conn.RemoteAddr())
+	defer conn.Close()
+
+	for {
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		// リクエストを読み込む
+		request, err := http.ReadRequest(bufio.NewReader(conn))
+		if err != nil {
+			neterr, ok := err.(net.Error)
+			if ok && neterr.Timeout() {
+				fmt.Println("Timeout")
+				break
+			} else if err == io.EOF {
+				break
+			}
+		}
+		dump, err := httputil.DumpRequest(request, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+
+		response := http.Response{
+			StatusCode: 200,
+			ProtoMajor: 1,
+			ProtoMinor: 1,
+			Header:     make(http.Header),
+		}
+		if isGZipAcceptable(request) {
+			content := "Hello World (gzipped)\n"
+			// コンテンツを gzip 化して転送
+			var buffer bytes.Buffer
+			writer := gzip.NewWriter(&buffer)
+			io.WriteString(writer, content)
+			writer.Close()
+			response.Body = io.NopCloser(&buffer)
+			response.ContentLength = int64(buffer.Len())
+			response.Header.Set("Content-Encoding", "gzip")
+		} else {
+			content := "Hello World\n"
+			response.Body = io.NopCloser(strings.NewReader(content))
+			response.ContentLength = int64(len(content))
+		}
+		response.Write(conn)
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8888")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Server is running at localhost:8888")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go processSession(conn)
+	}
+}
+
 // =======================================================
 // ## 6.7.2 gzip 圧縮に対応したサーバー
 
