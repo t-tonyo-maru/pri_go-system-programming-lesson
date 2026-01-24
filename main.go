147f831b8aea634af8d6e449d2e8cceef6a5fd31
t-tonyo-maru <tonyomaru108@gmail.com>
2026-01-25 01:29:56 +0900

    update

--- main.go
+++ main.go
@@ -1,42 +1,45 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"net"
-	"net/http"
-	"net/http/httputil"
-	"os"
-	"path/filepath"
-)
-
 func main() {
-	conn, err := net.Dial("unix", filepath.Join(os.TempDir(), "unixdomainsocket-sample"))
-	if err != nil {
-		panic(err)
-	}
 
-	request, err := http.NewRequest("get", "http;//localhost:8888", nil)
-	if err != nil {
-		panic(err)
-	}
-
-	request.Write(conn)
-	response, err := http.ReadResponse(bufio.NewReader(conn), request)
-
-	if err != nil {
-		panic(err)
-	}
-
-	dump, err := httputil.DumpResponse(response, true)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(dump))
 }
 
 // =======================================================
 // ## 8.2.3 Unix ドメインソケット版の HTTP クライアントを作る
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"net"
+// 	"net/http"
+// 	"net/http/httputil"
+// 	"os"
+// 	"path/filepath"
+// )
+
+// func main() {
+// 	conn, err := net.Dial("unix", filepath.Join(os.TempDir(), "unixdomainsocket-sample"))
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	request, err := http.NewRequest("get", "http;//localhost:8888", nil)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	request.Write(conn)
+// 	response, err := http.ReadResponse(bufio.NewReader(conn), request)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	dump, err := httputil.DumpResponse(response, true)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println(string(dump))
+// }
 
 // =======================================================
 // ## 8.2.2 Unix ドメインソケット版の HTTP サーバーを作る
