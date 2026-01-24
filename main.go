df75d48aa3df841f6d62393d750397da8203353c
t-tonyo-maru <tonyomaru108@gmail.com>
2026-01-25 01:08:41 +0900

    update

--- main.go
+++ main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+// =======================================================
+// ## 7.2.2 クライアント側の実装例
+// import (
+// 	"fmt"
+// 	"net"
+// )
 
-func main() {
-	fmt.Println("hoge")
-}
+// func main() {
+// 	conn, err := net.Dial("udp4", "localhost:8888")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer conn.Close()
+// 	fmt.Println("Sending to server")
+
+// 	_, err = conn.Write([]byte("Hello from Client"))
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println("Receiving from server")
+// 	buffer := make([]byte, 1500)
+// 	length, err := conn.Read(buffer)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Printf("Received: %s\n", string(buffer[:length]))
+// }
+
+// =======================================================
+// ## 7.2.1 サーバー側の実装例
+// import (
+// 	"fmt"
+// 	"net"
+// )
+
+// func main() {
+// 	fmt.Println("Server is running at localhost:8888")
+// 	conn, err := net.ListenPacket("udp", "localhost:8888")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer conn.Close()
+
+// 	buffer := make([]byte, 1500)
+// 	for {
+// 		length, remoteAddress, err := conn.ReadFrom(buffer)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		fmt.Printf("Reveived from %v: %v\n", remoteAddress, string(buffer[:length]))
+
+// 		_, err = conn.WriteTo([]byte("Hello from Server"), remoteAddress)
+
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 	}
+// }
