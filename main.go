6b984723a47d95d4878ebc6e5210fa7714ca4a44
t-tonyo-maru <tonyomaru108@gmail.com>
2026-01-25 23:45:02 +0900

    update

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
+// 新規作成
+func open() {
+	file, err := os.Create("textfile.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	io.WriteString(file, "New file content\n")
 }
+
+// 読み込み
+func read() {
+	file, err := os.Open("textfile.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	fmt.Println("Read file:")
+	io.Copy(os.Stdout, file)
+}
+
+func main() {
+	open()
+	read()
+}
+
+// =======================================================
+// ## 9.2.1 ファイル作成/読み込み
