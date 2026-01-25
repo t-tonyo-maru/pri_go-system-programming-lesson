7c8f11849d8fa3c274c379650f4d4bc8e4ba0d4c
t-tonyo-maru <tonyomaru108@gmail.com>
2026-01-25 12:42:51 +0900

    update

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+)
 
+func main() {
+	path := filepath.Join(os.TempDir(), "unixdomainsocket-server")
+	// エラーチェックは削除（存在しなかったらしかなったで問題ないので不要）
+	os.Remove(path)
+	fmt.Println("Server is running at " + path)
+
+	conn, err := net.ListenPacket("unixgram", path)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	buffer := make([]byte, 1500)
+	for {
+		length, remoteAddress, err := conn.ReadFrom(buffer)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Revieved from %v: %v\n", remoteAddress, string(buffer[:length]))
+		_, err = conn.WriteTo([]byte("Hello from Server"), remoteAddress)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 // =======================================================
