6f2f9d0c4a02579721c8eaebb34fb022f9065fb2
t-tonyo-maru <tonyomaru108@gmail.com>
2026-01-26 00:43:36 +0900

    update

--- main.go
+++ main.go
@@ -5,6 +5,40 @@ func main() {
 }
 
 // =======================================================
+// ## 9.2.5 ファイルの属性の取得
+// import (
+// 	"fmt"
+// 	"os"
+// )
+
+// /**
+//  * コマンド: go run main.go README.md
+//  */
+// func main() {
+// 	if len(os.Args) == 1 {
+// 		fmt.Printf("%s [exec file name]", os.Args[0])
+// 		os.Exit(1)
+// 	}
+
+// 	info, err := os.Stat(os.Args[1])
+// 	if err == os.ErrNotExist {
+// 		fmt.Printf("file not found: %s\n", os.Args[1])
+// 	} else if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Println("FileInfo")
+// 	fmt.Printf("  ファイル名: %v\n", info.Name())
+// 	fmt.Printf("  サイズ: %v\n", info.Size())
+// 	fmt.Printf("  変更日時: %v\n", info.ModTime())
+// 	fmt.Printf("Mode()")
+// 	fmt.Printf("  ディレクトリ?: %v\n", info.Mode().IsDir())
+// 	fmt.Printf("  読み書き可能な通常ファイル?: %v\n", info.Mode().IsRegular())
+// 	fmt.Printf("  Unix のファイルアクセス権限ビット?: %o\n", info.Mode().Perm())
+// 	fmt.Printf("  モードのテキスト表現?: %v\n", info.Mode().String())
+// }
+
+// =======================================================
 // ## 9.2.4 ファイルの削除/移動/リネーム
 // import (
 // 	"io"
