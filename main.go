7df5c91087ff4ed5c136c366c262e433ea73dcc4
t-tonyo-maru <tonyomaru108@gmail.com>
2026-01-25 23:59:25 +0900

    update

--- main.go
+++ main.go
@@ -1,49 +1,119 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"os"
-)
-
-// 新規作成
-func open() {
-	file, err := os.Create("textfile.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	io.WriteString(file, "New file content\n")
-}
-
-// 読み込み
-func read() {
-	file, err := os.Open("textfile.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	fmt.Println("Read file:")
-	io.Copy(os.Stdout, file)
-}
-
-// 追記モード
-func append() {
-	file, err := os.OpenFile("textfile.txt", os.O_RDWR|os.O_APPEND, 0666)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-	io.WriteString(file, "Appended content\n")
-}
-
 func main() {
-	open()
-	append()
-	read()
+
 }
 
 // =======================================================
+// ## 9.2.4 ファイルの削除/移動/リネーム
+// import (
+// 	"math/rand"
+// 	"os"
+// )
+
+// func getString() string {
+// 	str := ""
+
+// 	strings := []string{
+// 		"a", "b", "c", "d", "e", "f", "g",
+// 	}
+
+// 	for i := 0; i < 30000; i++ {
+// 		str += strings[rand.Intn(len(strings)-1)]
+// 	}
+
+// 	return str
+// }
+
+// func main() {
+// 	file, err := os.Create("server.log")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	file.WriteString(getString())
+
+// 	// 先頭 100 バイトで切る
+// 	os.Truncate("server.log", 100)
+// 	// Truncateメソッドを利用する場合
+// 	file.Truncate(100)
+// }
+
+// =======================================================
+// import "os"
+
+// func create() {
+// 	_, err := os.Create("server.log")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	os.Mkdir("workdir", 0755)
+// }
+
+// func remove() {
+// 	os.Remove("server.log")
+// 	os.Remove("workdir")
+// }
+
+// func main() {
+// 	create()
+// 	remove()
+// }
+
+// =======================================================
+// ## 9.2.3 ディレクトリの作成
+// import "os"
+
+// func main() {
+// 	// フォルダを1階層だけ作成
+// 	os.Mkdir("setting", 0755)
+// 	// 深いフォルダを1回で作成
+// 	os.MkdirAll("setting/myapp/networksettings", 0755)
+// }
+
+// =======================================================
 // ## 9.2.1 ファイル作成/読み込み
+// import (
+// 	"fmt"
+// 	"io"
+// 	"os"
+// )
+
+// // 新規作成
+// func open() {
+// 	file, err := os.Create("textfile.txt")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer file.Close()
+
+// 	io.WriteString(file, "New file content\n")
+// }
+
+// // 読み込み
+// func read() {
+// 	file, err := os.Open("textfile.txt")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer file.Close()
+
+// 	fmt.Println("Read file:")
+// 	io.Copy(os.Stdout, file)
+// }
+
+// // 追記モード
+// func append() {
+// 	file, err := os.OpenFile("textfile.txt", os.O_RDWR|os.O_APPEND, 0666)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer file.Close()
+// 	io.WriteString(file, "Appended content\n")
+// }
+
+// func main() {
+// 	open()
+// 	append()
+// 	read()
+// }
