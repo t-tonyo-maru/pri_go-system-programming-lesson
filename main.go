04a8ee13b43d68d22cde6e434b1d1b3661605da3
t-tonyo-maru <tonyomaru108@gmail.com>
2026-01-31 01:20:46 +0900

    update

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/billziss-gh/cgofuse/fuse"
 	"gocloud.dev/blob"
@@ -15,6 +16,29 @@ import (
 type CloudFileSystem struct {
 	fuse.CloudFileSystem
 	bucket *blob.Bucket
+}
+
+func (cf *CloudFileSystem) Getattr(path string, stat *fuse.Satt_t, fh int64) (errc int) {
+	if path == "/" {
+		stat.Mode = fuse.S_IFDIR | 0555
+		return 0
+	}
+	ctx := context.Background()
+	name := strings.TrimLeft(path, "/")
+	a, err := cf.bucket.Attributes(ctx, name)
+	if err != nil {
+		_, err := cf.bucket.Attributes(ctx, name+"/")
+		if err != nil {
+			return -fuse.ENOENT
+		}
+		stat.Mode = fuse.S_IFDIR | 0555
+	} else {
+		stat.Mode = fuse.S_IFREG | 0444
+		stat.Size = a.Size
+		stat.Mtim = fuse.NewTimeSpec(a.ModTime)
+	}
+	stat.Nlink = 1
+	return 0
 }
 
 func main() {
