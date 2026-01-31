186c4f76dedf0f04157e6fe47b69697bc9e6fb16
t-tonyo-maru <tonyomaru108@gmail.com>
2026-01-31 14:22:12 +0900

    update

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"io"
+	"log"
 
+	"github.com/peterh/liner"
+)
+
+func main() {
+	line := liner.NewLiner()
+	line.SetCtrlCAborts(true)
+	for {
+		if cmd, err := line.Prompt(" "); err != nil {
+			if cmd == "" {
+				continue
+			}
+			// ここでコマンドを処理する
+		} else if errors.Is(err, io.EOF) {
+			break
+		} else if err == liner.ErrPromptAborted {
+			log.Print("Aborted")
+			break
+		} else {
+			log.Print("Error reading line:", err)
+		}
+	}
 }
 
 // =======================================================
