59223034a983d58593b99c384e7221498f50baee
t-tonyo-maru <tonyomaru108@gmail.com>
2026-02-01 15:39:57 +0900

    update

--- main.go
+++ main.go
@@ -1,7 +1,94 @@
 package main
 
 // =======================================================
+// ## 14.7.3 sync.Once
+
+// =======================================================
+// ## 14.7.3 sync.Once
+// import (
+// 	"fmt"
+// 	"sync"
+// )
+
+// func initialize() {
+// 	fmt.Println("初期化処理")
+// }
+
+// var once sync.Once
+
+// func main() {
+// 	// 3回呼び出しても一度しか呼ばれない
+// 	once.Do(initialize)
+// 	once.Do(initialize)
+// 	once.Do(initialize)
+// }
+
+// =======================================================
+// ## 14.7.2 sync.WaitGroup
+// import (
+// 	"fmt"
+// 	"sync"
+// 	"time"
+// )
+
+// func main() {
+// 	var wg sync.WaitGroup
+
+// 	// ジョブ数をあらかじめ登録
+// 	wg.Add(2)
+
+// 	go func() {
+// 		// 非同期で仕事をする
+// 		fmt.Println("仕事1")
+// 		// 数秒待機
+// 		time.Sleep(time.Second * 2)
+// 		// Done で完了を通知
+// 		wg.Done()
+// 	}()
+
+// 	go func() {
+// 		// 自動機で字ごとをする(2)
+// 		fmt.Println("仕事2")
+// 		// 数秒待機
+// 		time.Sleep(time.Second * 5)
+// 		// Done で完了を通知
+// 		wg.Done()
+// 	}()
+
+// 	// すべての処理が終了するまで待つ
+// 	wg.Wait()
+// 	fmt.Println("終了")
+// }
+
+// =======================================================
 // ## 14.7.1 sync.Mutex / sync.RWMutex
+// import (
+// 	"fmt"
+// 	"sync"
+// )
+
+// var id int
+
+// func generateId(mutex *sync.Mutex) int {
+// 	// Lock() / Unlock() をペアで呼び出してブロックする
+// 	mutex.Lock()
+// 	id++
+// 	result := id
+// 	mutex.Unlock()
+// 	return result
+// }
+
+// func main() {
+// 	// sync.Mutex 構造体の変数宣言
+// 	// 次の宣言をしてもポインタ型になるだけで正常に動作する
+// 	// mutex := new(sync.Mutex)
+// 	var mutex sync.Mutex
+// 	for i := 0; i < 100; i++ {
+// 		go func() {
+// 			fmt.Printf("id: %d\n", generateId(&mutex))
+// 		}()
+// 	}
+// }
 
 // =======================================================
 // ## 14.2.1 goroutine と情報共有
