0b76173ea35e70a5e5986fb6ef9ae2abdf4cf0b6
t-tonyo-maru <tonyomaru108@gmail.com>
2026-02-02 00:05:51 +0900

    update

--- main.go
+++ main.go
@@ -2,6 +2,50 @@ package main
 
 // =======================================================
 // ## 15.2.8 依存関係のあるタスクを表現する Future/Promise
+// import (
+// 	"fmt"
+// 	"os"
+// 	"strings"
+// )
+
+// func readFile(path string) chan string {
+// 	// ファイルを読み込み、その結果を返す Future を返す
+// 	promise := make(chan string)
+// 	go func() {
+// 		content, err := os.ReadFile(path)
+// 		if err != nil {
+// 			fmt.Printf("read error %s\n", err.Error())
+// 			close(promise)
+// 		} else {
+// 			// 約束を果たした
+// 			promise <- string(content)
+// 		}
+// 	}()
+// 	return promise
+// }
+
+// func printFunc(futureSource chan string) chan []string {
+// 	// 文字列中の関数一覧を返す Future を返す
+// 	promise := make(chan []string)
+// 	go func() {
+// 		var result []string
+// 		// future が解決するまで待って待機
+// 		for _, line := range strings.Split(<-futureSource, "\n") {
+// 			if strings.HasPrefix(line, "func ") {
+// 				result = append(result, line)
+// 			}
+// 		}
+// 		// 約束を果たした
+// 		promise <- result
+// 	}()
+// 	return promise
+// }
+
+// func main() {
+// 	futureSource := readFile("future_promise.go")
+// 	futureFuncs := printFunc(futureSource)
+// 	fmt.Println(strings.Join(<-futureFuncs, "\n"))
+// }
 
 // =======================================================
 // ## 15.2.7 決まった数の goroutine でタスクを消化: ワーカープール
