76a4c71c9c1ec8ea6c640bc2f9ab044b103df562
t-tonyo-maru <tonyomaru108@gmail.com>
2026-02-02 00:13:40 +0900

    update

--- main.go
+++ main.go
@@ -1,7 +1,88 @@
 package main
 
 // =======================================================
+// ## 15.2.9 イベントの流れを定義する: ReactiveX
+
+// =======================================================
 // ## 15.2.8 依存関係のあるタスクを表現する Future/Promise
+// import (
+// 	"fmt"
+// 	"os"
+// 	"strings"
+// )
+
+// type StringFuture struct {
+// 	receiver chan string
+// 	cache    string
+// }
+
+// func NewStringFuture() (*StringFuture, chan string) {
+// 	f := &StringFuture{
+// 		receiver: make(chan string),
+// 	}
+// 	return f, f.receiver
+// }
+
+// func (f *StringFuture) Get() string {
+// 	r, ok := <-f.receiver
+// 	if ok {
+// 		close(f.receiver)
+// 		f.cache = r
+// 	}
+// 	return f.cache
+// }
+
+// func (f *StringFuture) Close() {
+// 	close(f.receiver)
+// }
+
+// func readFile(path string) *StringFuture {
+// 	// ファイルを読み込み、その結果を返す Future を返す
+// 	promise, future := NewStringFuture()
+// 	go func() {
+// 		content, err := os.ReadFile(path)
+// 		if err != nil {
+// 			fmt.Printf("read error %s\n", err.Error())
+// 		} else {
+// 			future <- string(content)
+// 		}
+// 	}()
+// 	return promise
+// }
+
+// func printFunc(futureSource *StringFuture) chan []string {
+// 	// 文字列中の関数一覧を返す Future を返す
+// 	promise := make(chan []string)
+// 	go func() {
+// 		var result []string
+// 		// future が解決するまで待って待機
+// 		for _, line := range strings.Split(futureSource.Get(), "\n") {
+// 			if strings.HasPrefix(line, "func ") {
+// 				result = append(result, line)
+// 			}
+// 		}
+// 		// 約束を果たした
+// 		promise <- result
+// 	}()
+// 	return promise
+// }
+
+// func countLines(futureSource *StringFuture) chan int {
+// 	promise := make(chan int)
+// 	go func() {
+// 		promise <- len(strings.Split(futureSource.Get(), "\n"))
+// 	}()
+// 	return promise
+// }
+
+// func main() {
+// 	futureSource := readFile("future_promise.go")
+// 	futureFuncs := printFunc(futureSource)
+// 	fmt.Println(strings.Join(<-futureFuncs, "\n"))
+// 	fmt.Println(<-countLines(futureSource))
+// }
+
+// =======================================================
 // import (
 // 	"fmt"
 // 	"os"
