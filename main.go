c7d9e430a8b197e8736ea8477fbff952f15cd5e8
t-tonyo-maru <tonyomaru108@gmail.com>
2026-02-01 23:59:49 +0900

    update

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
 // =======================================================
+// ## 15.2.8 依存関係のあるタスクを表現する Future/Promise
+
+// =======================================================
 // ## 15.2.7 決まった数の goroutine でタスクを消化: ワーカープール
+// import (
+// 	"fmt"
+// 	"runtime"
+// 	"sync"
+// )
+
+// // 元金均等
+// func calc(id, price int, interestRate float64, year int) {
+// 	months := year * 12
+// 	interest := 0
+// 	for i := 0; i < months; i++ {
+// 		balance := price * (months - i) / months
+// 		interest += int(float64(balance) * interestRate / 12)
+// 	}
+// 	fmt.Printf("year=%d total=%d interest=%d id=%d\n", year, price+interest, interest, id)
+// }
+
+// // ワーカー
+// func worker(id, price int, interestRate float64, years chan int, wg *sync.WaitGroup) {
+// 	// タスクがなくなってタスクのチャネルが close されるまで無限ループ
+// 	for year := range years {
+// 		calc(id, price, interestRate, year)
+// 		wg.Done()
+// 	}
+// }
+
+// func main() {
+// 	// 借入金
+// 	price := 400000000
+// 	// 利子 1.1%
+// 	interestRate := 0.011
+// 	// タスクは chan に格納
+// 	years := make(chan int, 35)
+// 	for i := 1; i < 36; i++ {
+// 		years <- i
+// 	}
+
+// 	var wg sync.WaitGroup
+// 	wg.Add(35)
+// 	// CPU コア数分の goroutine を起動
+// 	for i := 0; i < runtime.NumCPU(); i++ {
+// 		go worker(i, price, interestRate, years, &wg)
+// 	}
+// 	// すべてのワーカーが終了する
+// 	close(years)
+// 	wg.Wait()
+// }
 
 // =======================================================
 // ## 15.2.6 並列 for ループ
