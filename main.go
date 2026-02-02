4f8f75b26744fb543530de6347574015a255c2bc
t-tonyo-maru <tonyomaru108@gmail.com>
2026-02-03 00:27:12 +0900

    update

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 	"os"
+	"path/filepath"
 	"runtime"
 
 	"github.com/opencontainers/runc/libcontainer"
 	_ "github.com/opencontainers/runc/libcontainer/config"
+	"github.com/opencontainers/runc/libcontainer/configs"
 	_ "github.com/opencontainers/runc/libcontainer/nsenter"
-	_ "golang.org/x/sys/unix"
+	"golang.org/x/sys/unix"
 )
 
 func init() {
@@ -24,7 +26,145 @@ func init() {
 }
 
 func main() {
+	abs, _ := filepath.Abs("./")
+	factory, err := libcontainer.New(abs, libcontainer.Cgroupfs, libcontainer.InitArgs(os.Args[0], "init"))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
+	capabilities := []string{
+		"CAP_CHOWN",
+		"CAP_DAC_OVERRIDE",
+		"CAP_FSETID",
+		"CAP_FOWNER",
+		"CAP_MKNOD",
+		"CAP_NET_RAW",
+		"CAP_SETGID",
+		"CAP_SETUID",
+		"CAP_SETFCAP",
+		"CAP_SETPCAP",
+		"CAP_NET_BIND_SERVICE",
+		"CAP_SYS_CHROOT",
+		"CAP_KILL",
+		"CAP_AUDIT_WRITE",
+	}
+	defaultMountFlags := unix.MS_NOEXEC | unix.MS_NOSUID | unix.MS_NODEV
+	config := &configs.Config{
+		Roofs: abs + "/roofs",
+		Capabilities: &configs.Capabilities{
+			Bounding:    capabilities,
+			Effective:   capabilities,
+			Inheritable: capabilities,
+			Permitted:   capabilities,
+			Ambient:     capabilities,
+		},
+		Namespaces: configs.Namespaces([]configs.Namespace{
+			{Type: configs.NEWNS},
+			{Type: configs.NEWUTS},
+			{Type: configs.NEWIPC},
+			{Type: configs.NEWPID},
+			{Type: configs.NEWNET},
+		}),
+		Cgroups: &configs.Cgroup{
+			Name:   "test-container",
+			Parent: "system",
+			Resources: &configs.Resources{
+				MemorySwappiness: nil,
+				AllowAllDevices:  nil,
+				AllowedDevices:   configs.DefaultAllowedDevices,
+			},
+		},
+		MaskPaths: []string{"/proc/kcore", "/sys/firmare"},
+		ReadonlyPaths: []string{
+			"/proc/sys", "proc/sysrq-trigger", "/proc/irq", "/proc/bus",
+		},
+		Devices: configs.DefaultAutoCreateDevices,
+		Mounts: []*configs.Mount{
+			{
+				Source:      "proc",
+				Destination: "/proc",
+				Device:      "proc",
+				Flags:       defaultMountFlags,
+			},
+			{
+				Source:      "tmpfs",
+				Destination: "/dev",
+				Device:      "tmpfs",
+				Flags:       unix.MS_NOSUID | unix.MS_STRICTATIME,
+				Data:        "mode=755",
+			},
+			{
+				Source:      "devpts",
+				Destination: "/dev/pts",
+				Device:      "devpts",
+				Flags:       unix.MS_NOSUID | unix.MS_NOEXEC,
+				Data:        "netinstance,ptmxmode=0666,mode=0620,gid=5",
+			},
+			{
+				Source:      "tmpfs",
+				Source:      "shm",
+				Destination: "/dev/shm",
+				Data:        "mode=1777,size=65535k",
+				Flags:       defaultMountFlags,
+			},
+			{
+				Source:      "mqueue",
+				Destination: "/dev/mqueue",
+				Device:      "mqueue",
+				Flags:       defaultMountFlags,
+			},
+			{
+				Source:      "sysfs",
+				Destination: "/sys",
+				Device:      "sysfs",
+				Flags:       defaultMountFlags | unix.MS_RDONLY,
+			},
+		},
+		Networks: []*configs.Network{
+			{
+				Type:    "looppack",
+				Address: "127.0.0.1/0",
+				Gateway: "localhost",
+			},
+		},
+		Rlimits: []configs.Rlimit{
+			{
+				Type: unix.RLIMIT_NOFILE,
+				Hard: uint64(1025),
+				Soft: uint64(1025),
+			},
+		},
+	}
+
+	container, err := factory.Create("container-id", config)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	process := &libcontainer.Process{
+		Args:   []string{"/bin/sh"},
+		Env:    []string{"PATH=/bin"},
+		User:   "root",
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+
+	err = container.Run(process)
+	if err != nil {
+		container.Destroy()
+		log.Fatal(err)
+		return
+	}
+
+	_, err = process.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	container.Destroy()
 }
 
 // =======================================================
